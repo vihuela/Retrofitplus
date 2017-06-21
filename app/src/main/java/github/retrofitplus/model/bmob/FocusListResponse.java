742081bf08db981855d281a1395ecf966865f13f
ricky <ricky@worldgo.net>
2017-06-21 09:51:50 +0800

    first commit

--- app/src/main/java/github/retrofitplus/model/bmob/FocusListResponse.java
+++ app/src/main/java/github/retrofitplus/model/bmob/FocusListResponse.java
@@ -1,6 +1,7 @@
 package github.retrofitplus.model.bmob;
 
 
+//RxCache缓存没有保存泛型，所以Response为显性类型
 public class FocusListResponse extends BaseResponse<FocusListResponse.FocusListItem> {
 
     public static class FocusListItem {
